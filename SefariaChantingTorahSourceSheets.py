c49c6525a4e27392e487a087a31b25a605469b5c
Valerie Monaco <vmonaco@24dcaea7d349>
2021-07-11 20:37:16 +0000

    dummy code for future db listing

--- SefariaChantingTorahSourceSheets.py
+++ SefariaChantingTorahSourceSheets.py
@@ -5,10 +5,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+s_sheets = [
+    {
+        'Torah_Verses':'Exodus 1:10',
+        'Created_By': 'vmonaco',
+        'Created_On': 'July 11, 2021'
+        },
+    {
+        'Torah_Verses':'Genesis 2:11-13',
+        'Created_By': 'amp',
+        'Created_On': 'May 8, 2021'
+        },
+    {
+        'Torah_Verses':'Numbers 5:23-24',
+        'Created_By': 'dpolk',
+        'Created_On': 'March 1, 2021'
+        }
+    ]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', s_sheets=s_sheets)
 
 @app.route('/about')
 def about():
