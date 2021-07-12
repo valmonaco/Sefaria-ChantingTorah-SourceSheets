25d9e41ce320fae3bedf401439b9f14a9b19b219
Valerie Monaco <vmonaco@24dcaea7d349>
2021-07-12 00:05:08 +0000

    Add Nav Bar and CSS

--- SefariaChantingTorahSourceSheets.py
+++ SefariaChantingTorahSourceSheets.py
@@ -1,7 +1,4 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
