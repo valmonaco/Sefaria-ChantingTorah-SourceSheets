3febc353729ea658b4f6e455783fd8c37388efa0
Valerie Monaco <vmonaco@24dcaea7d349>
2021-07-15 17:08:27 +0000

    Initial forms and form templates

--- SefariaChantingTorahSourceSheets.py
+++ SefariaChantingTorahSourceSheets.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+
 
 s_sheets = [
     {
@@ -27,4 +30,13 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form=RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
