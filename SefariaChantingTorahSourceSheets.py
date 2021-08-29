da7e4213a26f84461497c830f10d65f65ebaf59d
Valerie Monaco <vmonaco@24dcaea7d349>
2021-08-29 20:55:42 +0000

    Move form to home page

--- SefariaChantingTorahSourceSheets.py
+++ SefariaChantingTorahSourceSheets.py
@@ -11,47 +11,10 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 recaptcha = ReCaptcha(app) # Create a ReCaptcha object by passing in 'app' as parameter
 
-s_sheets = [
-    {
-        'Torah_Verses':'Exodus 1:10',
-        'Created_By': 'vmonaco',
-        'Created_On': 'July 11, 2021'
-        },
-    {
-        'Torah_Verses':'Genesis 2:11-13',
-        'Created_By': 'amp',
-        'Created_On': 'May 8, 2021'
-        },
-    {
-        'Torah_Verses':'Numbers 5:23-24',
-        'Created_By': 'dpolk',
-        'Created_On': 'March 1, 2021'
-        }
-    ]
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET','POST'])
+@app.route('/home',methods=['GET','POST'])
 def home():
-    return render_template('home.html', s_sheets=s_sheets)
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
-@app.route('/register', methods=['GET','POST'])
-def register():
-    form=RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}.','success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Login')
-
-@app.route('/create', methods=['GET','POST'])
-def create():
     form=CreateForm()
 
     if request.method == 'POST': # Check to see if flask.request.method is POST
@@ -72,5 +35,21 @@ def create():
                 #return redirect(url_for('home'))
         else:
                 flash('If you are not a robot, go ahead and check the box.')
-    return render_template('create.html', title='Create Source Sheet', form=form)
+    return render_template('home.html', title='Create Source Sheet', form=form)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.','success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
 
